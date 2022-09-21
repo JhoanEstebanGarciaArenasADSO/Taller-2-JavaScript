9b2bfa54c8a43fc4cad28f1d50f8b7d0cf2ac9fe
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-21 09:03:36 -0500

    Ejercicio 2 Parte 2

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 1. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
-// por favor utilizar funciones matemáticas de Python para ello 
-// d = rc((x2-x1)^2 + (y2-y1)^2)
+// 2. Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de
+// descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total
+// de la compra.
 
-let x1= Number(prompt("Ingrese el valor de x1: "));
-let x2= Number(prompt("Ingrese el valor de x2: "));
-let y1= Number(prompt("Ingrese el valor de y1: "));
-let y2= Number(prompt("Ingrese el valor de y2: "));
-let dis = Math.sqrt((x2-x1)**2 + (y2-y1));
-console.log(`La distancia entre los dos puntos es: ${dis}`);
+
+let pro= Number(prompt("Ingrese el precio del producto 1: "));
+let pro2= Number(prompt("Ingrese el precio del producto 2: "));
+let pro3= Number(prompt("Ingrese el precio del producto 3: "));
+let pro4= Number(prompt("Ingrese el precio del producto 4: "));
+let pro5= Number(prompt("Ingrese el precio del producto 5: "));
+let desc1= pro*0.05; 
+let desc2= pro2*0.05; 
+let desc3 = pro4*0.02;
+let desc4 = pro5*0.02;
+let val1 = pro-desc1;
+let val2 = pro2-desc2;
+let val3 = pro4-desc3;
+let val4 = pro5-desc4;
+let total= val1+val2+val3+val4+pro3;
+console.log(`El valor a pagar del producto 1 es: ${val1}`);
+console.log(`El valor a pagar del producto 2 es: ${val2}`);
+console.log(`El valor a pagar del producto 4 es: ${val3}`);
+console.log(`El valor a pagar del producto 5 es: ${val4}`);
+console.log(`El total a pagar es: ${total}`);
 })
