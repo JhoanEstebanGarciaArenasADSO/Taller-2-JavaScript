d9ab8077f9533fde0294e798975ff57f7d822bdb
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-21 11:16:49 -0500

    Ejercicio 4 Parte 2

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 3. Calcular la edad de una madre en el momento de dio a luz a alguno de sus hijos.
-// • Generar el algoritmo que dé como resultado el tiempo estimado para el llenado de un
-// depósito.
-// • Q=V/t, siendo Q (caudal), V (volumen) y t (tiempo).
-// • Volumen = PI *(radio^2)* H (altura del depósito)
-// • El tiempo se encuentra en minutos.
-// • Normalmente se mide el volumen en litros y el tiempo en segundos.
+// 4. Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por
+// $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le
+// sobran $91000. ¿cuánto dinero tenía?
 
-
-let tie= Number(prompt("Ingrese el tiempo: "));
-let h= Number(prompt("Ingrese la altura: "));
-let rad= Number(prompt("Ingrese el radio: "));
-let vol= Math.PI * (rad**2)* h; 
-let q= vol/tie; 
-console.log(`El volumen es: ${vol}`);
-console.log(`El Caudal es: ${q}`);
+let llaves=11500*5;
+let bomba=1168000;
+let cajas=87000*3;
+let dinerosobrante=91000;
+let din=llaves+bomba+cajas;
+let dint=din+dinerosobrante;
+console.log(`El precio que pago era: ${din}`);
+console.log(`El dinero que tenia era: ${dint}`);
 })
