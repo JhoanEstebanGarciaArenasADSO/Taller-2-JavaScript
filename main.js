7e85daf39da721cf5234d2ceab88fbdef2d00af6
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-21 11:51:59 -0500

    Ejercicio 7 Parte 2

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 6. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg
-// transporta ?
+// 7. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
+// 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
+// Lorena?
 
-let cajas=25;
-let pesoc=748;
-let pesot=cajas*pesoc;
-console.log(`El camion transporta ${pesot} kilos`);
+let tapas=1;
+let martin=2/3;
+let jairo= 1/4;
+let lorena= tapas-martin - jairo;
+console.log(`A Lorena le corresponde ${lorena} de tapas`);
 })
