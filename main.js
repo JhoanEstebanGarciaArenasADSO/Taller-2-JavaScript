74d717943fbfa2e643add3f71f1c15a1981acdb7
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-28 11:20:44 -0500

    Ejercicio 10 Parte 2

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 9. Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
-// pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
-// que representen el algoritmo para solucionar este problema.
-// Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula
-// sostenida y la edad del entrevistado.
-let name = prompt("Ingrese su nombre: ");
-let añon = Number(prompt("Ingrese su año de nacimiento: "));
-let añoe = 2022;
-let edad= añoe - añon;
-console.log(`La edad de ${name} es ${edad} `);
+// 10. Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase
+// invertida.
+let frase = "No sabia que podia hacer eso";
+let frasi = frase.split("").reverse().join("")
+console.log(`${frasi} `);
 })
