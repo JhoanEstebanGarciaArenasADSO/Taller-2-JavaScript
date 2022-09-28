a0d7a4a1b41ef63255f5383802e650c810abecca
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-28 11:03:53 -0500

    Ejercicio 9 Parte 2

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 8. Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura. Considere que
-// se cobra por m2 y realice el código que representen el algoritmo que le permita ir generando
-// presupuestos para cada cliente.
-let metro = Number(prompt("Ingrese los metros cuadrados que quiere pintar: "))
-let preciom=50000;
-let pre=metro * preciom;
-console.log(`El valor que debe pagar es ${pre} `);
+// 9. Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
+// pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
+// que representen el algoritmo para solucionar este problema.
+// Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula
+// sostenida y la edad del entrevistado.
+let name = prompt("Ingrese su nombre: ");
+let añon = Number(prompt("Ingrese su año de nacimiento: "));
+let añoe = 2022;
+let edad= añoe - añon;
+console.log(`La edad de ${name} es ${edad} `);
 })
