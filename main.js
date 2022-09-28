224da04b27a28f5bb3832706e9ae69ecf2fc273b
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-28 10:51:56 -0500

    Ejercicio 8 Parte 2

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 7. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
-// 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
-// Lorena?
-
-let tapas=1;
-let martin=2/3;
-let jairo= 1/4;
-let lorena= tapas-martin - jairo;
-console.log(`A Lorena le corresponde ${lorena} de tapas`);
+// 8. Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura. Considere que
+// se cobra por m2 y realice el código que representen el algoritmo que le permita ir generando
+// presupuestos para cada cliente.
+let metro = Number(prompt("Ingrese los metros cuadrados que quiere pintar: "))
+let preciom=50000;
+let pre=metro * preciom;
+console.log(`El valor que debe pagar es ${pre} `);
 })
